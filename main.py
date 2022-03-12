2c8abe8ea81002ab0597e626851b42512b6fc5cd
Kevin Escoto <kevinescoto@Kevins-MacBook-Pro.local>
2022-03-12 16:18:02 -0500

    Fixed Continue Issue

--- main.py
+++ main.py
@@ -11,40 +11,48 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def power(x, y):
+    return x ** y
+
 
 print("Select Operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Power")
 
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in (1, 2, 3, 4, 5):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        if choice == '1':
+        if choice == 1:
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
+        elif choice == 2:
             print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
+        elif choice == 3:
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
+        elif choice == 4:
             print(num1, "/", num2, "=", divide(num1, num2))
+
+        elif choice == 5:
+            print(num1, "^", num2, "=", power(num1, num2))
+             
         
         # check if user wants another calculation
         # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
+        move_forward = input("Continue? (1 for yes/0 for no): ")
+        if move_forward == 0:
+            break
     
     else:
         print("Invalid Input")
